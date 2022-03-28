900f6fd9e389b7208ef11cca7250004d34e72a73
ManarAbed <manarabed333@gmail.com>
2022-03-28 16:36:17 +0300

    Desgin Pages

--- public/js/dom.js
+++ public/js/dom.js
@@ -37,14 +37,22 @@ const getAllPosts = (data) => {
       console.log(post);
       const PostDiv = document.querySelector(".post-div")
 
+      const userimg = document.createElement('img');
+      userimg.src = 'images/communityIcon_9jufwkeep7f41.jpeg';
+      userimg.className = 'placeholder-img';
+
+      const userinfo = document.createElement('div');
+      userinfo.className = 'user-info';
+
       const h4 = document.createElement('h4');
-      h4.textContent = post.name;
+      h4.innerHTML = `<span class="post-span">Posted by </span> ${post.name}`;
   
       const h3 = document.createElement('h3');
       h3.textContent = post.title;
     
       const p = document.createElement('p');
       p.textContent = post.content;
+      p.className = 'post-content';
   
       const img = document.createElement('img');
       img.src = post.img_url;
@@ -55,20 +63,19 @@ const getAllPosts = (data) => {
       const PostInfo = document.createElement('div');
       PostInfo.className = 'PostInfo';
 
-      const uparrow = document.createElement('i');
-      uparrow.className = 'bx bx-up-arrow-alt';
+      const uparrow = document.createElement('img');
+      uparrow.className = 'img-uparrow';
+      uparrow.src = 'images/icons8-up-arrow-100.png';
 
-
-
-      const downarrow = document.createElement('i');
-      downarrow.className = 'bx bx-down-arrow-alt';
+      const downarrow = document.createElement('img');
+      downarrow.className = 'img-downarrow';
+      downarrow.src = 'images/icons8-down-arrow-100.png';
 
       const voteNum = document.createElement('p');
       voteNum.textContent = post.votes_num;
 
       const section = document.createElement('section');
       section.className = 'post-sec';
-      
 
       uparrow.addEventListener('click' ,(e)=>{
         const voteVal = {
@@ -96,11 +103,14 @@ const getAllPosts = (data) => {
           voteNum.textContent = data.post[0].votes_num
         }).catch((err) => console.log(err))
       })
+      userinfo.appendChild(userimg);
+      userinfo.appendChild(h4);
 
-      PostInfo.appendChild(h4);
+      PostInfo.appendChild(userinfo);
       PostInfo.appendChild(h3);
-      PostInfo.appendChild(img);
       PostInfo.appendChild(p);
+      PostInfo.appendChild(img);
+
 
       section.appendChild(arrowVote);
       section.appendChild(PostInfo);
@@ -129,12 +139,41 @@ if(data[0].title !== null && data[0].content !== null && data[0].img_url !== nul
 
     if(data[i].title !== "" && data[i].content !== "") {
       const section = document.createElement('section');
+      section.className = 'post-sec'
+
       const h3 = document.createElement('h3');
       h3.textContent = userPost.title;
 
-      const deleteBtn = document.createElement('button');
-      deleteBtn.textContent = 'delete';
+      const deleteBtn = document.createElement('img');
+      deleteBtn.src = '../images/icons8-delete-100.png';
+      deleteBtn.className = 'deleteImg';
 
+      const p = document.createElement('p');
+      p.textContent = userPost.content;
+      p.className = 'post-content';
+
+      const arrowVote = document.createElement('div');
+      arrowVote.className = 'arrowVotes';
+
+      const PostInfo = document.createElement('div');
+      PostInfo.className = 'PostInfo';
+
+      const uparrow = document.createElement('img');
+      uparrow.className = 'img-uparrow';
+      uparrow.src = '../images/icons8-up-arrow-100.png';
+
+      const downarrow = document.createElement('img');
+      downarrow.className = 'img-downarrow';
+      downarrow.src = '../images/icons8-down-arrow-100.png';
+
+      const voteNum = document.createElement('p');
+     voteNum.textContent = userPost.votes_num;
+
+      PostInfo.appendChild(h3);
+      PostInfo.appendChild(deleteBtn);
+      PostInfo.appendChild(p);
+
+      
       deleteBtn.addEventListener('click', () =>{
         fetch(`/posts/delete/${data[i].postid}`, {
             method: 'DELETE'
@@ -147,20 +186,20 @@ if(data[0].title !== null && data[0].content !== null && data[0].img_url !== nul
         }).catch(err => console.log(err));
 
         section.remove();
-      })
-
-      const p = document.createElement('p');
-      p.textContent = userPost.content;
+      });
 
       if(data[i].img_url !== ""){
         const img = document.createElement('img');
         img.src = userPost.img_url;
-        section.appendChild(img);
+        PostInfo.appendChild(img);
       }
 
-      section.appendChild(h3);
-      section.appendChild(p);
-      section.appendChild(deleteBtn);
+      section.appendChild(arrowVote);
+      section.appendChild(PostInfo);
+
+      arrowVote.appendChild(uparrow);
+      arrowVote.appendChild(voteNum);
+      arrowVote.appendChild(downarrow);
 
       PostDiv.appendChild(section);
     }
@@ -168,6 +207,7 @@ if(data[0].title !== null && data[0].content !== null && data[0].img_url !== nul
   }else{
       const pNoPost = document.createElement('p');
       pNoPost.textContent = `hmm... u/${data[0].name} hasn't posted anything`;
+      pNoPost.className = 'nopost'
       PostDiv.appendChild(pNoPost);
   }
 }
--- server/database/queries/user/getUserById.js
+++ server/database/queries/user/getUserById.js
@@ -1,7 +1,7 @@
 const connection = require('../../config/connection');
 
 const getUserByIDQu = (id) => connection.query({
-  text: `select u.id , u.name , u.email , p.title , p.id as postId, p.content , p.img_url 
+  text: `select u.id , u.name , u.email , p.title , p.id as postId, p.content , p.img_url ,p.votes_num
   from users as u
   left join posts as p on u.id = p.userId
   where u.id= $1;`,
